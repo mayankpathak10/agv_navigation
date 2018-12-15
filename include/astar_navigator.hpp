fb1b9f956420da261938ec17bf027751fe7b20e5
bhargav-umd <bhargav@umd.edu>
2018-12-14 21:15:42 -0500

    [Task #9.1] [CODE]: Added header files in include folder to make Astar algorithm to work

--- include/astar_navigator.hpp
+++ include/astar_navigator.hpp
@@ -27,26 +27,65 @@
 #define INCLUDE_ASTAR_NAVIGATOR_HPP_
 
 #include <geometry_msgs/Twist.h>
-#include<ros/ros.h>
-#include<vector>
-#include<sensor_msgs/LaserScan.h>
+#include <ros/ros.h>
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+#include "../include/map.hpp"
+#include "../include/nodes.hpp"
 
 class astar_navigator {
-  public:
-    // default constructor
+ public:
+    int x_start_;
+    int y_start_;
+    int x_goal_;
+    int y_goal_;
+
+    /**
+     * @brief astar_navigator class Constructor
+     * @param none
+     * @return none
+     */
     astar_navigator();
-    // default destructor
-    ~astar_navigator();
 
-    vector astar(std::vector<std::vector<int4d>>);
+    /**
+     * @brief Parameterized astar_navigator Constructor
+     *
+     * @param x coordinate of the start node
+     * @param y coordinate of the start node
+     * @param x coordinate of the goal node
+     * @param y coordinate of the goal node
+     *
+     * @return none
+     */
+    astar_navigator(int xStart, int yStart, int xGoal, int yGoal);
 
-  private:
-    std::vector<double> open_list_;
-    std::vector<double> closed_list_;
-    double row;
-    double column;
-    std::vector<double> dest;
-    std::vector<double> source;
+    /**
+     *
+     * @brief Get an optimal path from start to goal node.
+     *
+     * @param vec is a map which holds info if a node is walkable or blocked
+     *
+     * @return path with parent directions from start to goal
+     */
+    std::vector<std::pair<int, int> > astar_path(
+        std::vector<std::vector<int> > map);
+
+    /**
+     *
+     * @brief overloads the '>' operator
+     *
+     * Gives priority to node with greater total cost
+     * to rearrange the priority Queue.
+     *
+     * @param p is a reference of a node
+     * @param q is a reference of a node
+     *
+     * @return true if the total cost (f cost) of p is greater, else false
+     *
+     */
+    friend bool operator>(const nodes& node1, const nodes& node2);
 };
-
-#endif  // INCLUDE_ASTAR_NAVIGATOR_HPP_
+#endif   // INCLUDE_ASTAR_HPP_
